28bd819d4b5fdb3f4da2be6db73db6e1da4b1096
camilono <orellanasolec@gmail.com>
2024-09-23 02:02:25 -0300

    inicio

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+console.log("ujsd")
+
+
+const div = document.querySelector("#barra")
+
+div.onclick = function() {
+    // Acciones que deseas realizar al hacer clic en la imagen
+    alert('Haz hecho clic en la imagen');
+
+    console.log("le hice clck")
+  };
+
+
+console.log(div)
